7e6c56fe4aec11cfcccfabe09a32b9bd350dd766
Kihong Kim <kimgihong2510@gmail.com>
2021-08-02 23:38:05 +0900

    add broacast

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import asyncio
 import time
-time.sleep(4)
 import datetime
 from pytz import timezone
 from discord.ext import tasks
@@ -9,7 +8,6 @@ import light_sensor as update
 import os
 from gtts import gTTS
 
-
 client = discord.Client()
 
 forbidend=datetime.datetime.now(timezone('Asia/Seoul'))
@@ -17,7 +15,7 @@ status=update.Openornot()
 
 token='ODU1ODY2Mjc5MDA0ODY0NTcz.YM4tlg.sdyBX-ggQThrEn0ziWcaQ2ApXRI'
 
-forbidenchannels=[546610833824940042,699572422734643261, 855787966945034290, 572759629029834772]
+forbidenchannels=[546610833824940042, 699572422734643261, 855787966945034290, 572759629029834772]
 
 
 def main():
@@ -58,7 +56,7 @@ def main():
         #closed 0, opened 1, forbidden 2
             
         if message.content == '?help':
-            await message.channel.send("?출입금지n (어드민 권한 필요)\n?컬방열렸나요?\n?컬방열어주세요")
+            await message.channel.send("?출입금지n (어드민 권한 필요)\n?컬방열렸나요?\n?컬방열어주세요\n?컬방공지 [내용]")
 
         if message.content == '?reboot':
             await message.channel.send("다시 시작합니다")
@@ -81,8 +79,6 @@ def main():
                    await message.channel.send("입력 형태가 올바르지 않습니다")
             else :
                 await message.channel.send("권한이 없습니다")
-
-
         
         if message.content == '?컬방열렸나요?':
             if status==1:
@@ -98,10 +94,28 @@ def main():
             if status==1:
                 await message.channel.send('띵동')
                 try:
-                    tts = gTTS(text=message.author.name + " 님께옵소서는 컬방에 친히 방문하셨사옵나이다!", lang='ko')
-                    tts.save("/home/pi/KertOpen/hell.mp3")
-                    os.system("mplayer /home/pi/KertOpen/hell.mp3")
-                    os.system("mplayer /home/pi/KertOpen/bell.mp3")
+                    tts = gTTS(text=3 * (message.author.name + " "), lang='ko')
+                    tts.save("hell.mp3")
+                    os.system("mplayer hell.mp3")
+                    os.system("mplayer bell.mp3")
+                except:
+                    await message.channel.send('초인종이 작동을 안해요 ㅠㅠ 다른 방법을 이용해주세요')
+
+            elif status==2:
+                forbidendstring=str(forbidend)
+                await message.channel.send(forbidendstring[0:19] + "까지 출입금지입니다")
+
+            else:
+                await message.channel.send('컬방이 닫혀있습니다')
+
+        if message.content[:5] == '?컬방공지':
+            broadcast = message.content[6:]
+            if status==1:
+                await message.channel.send(broadcast + '와 같이 보냈습니다')
+                try:
+                    tts = gTTS(text=message.author.name + " 님께서 알립니다. " + broadcast, lang='ko')
+                    tts.save("hell.mp3")
+                    os.system("mplayer hell.mp3")
                 except:
                     await message.channel.send('초인종이 작동을 안해요 ㅠㅠ 다른 방법을 이용해주세요')
 
